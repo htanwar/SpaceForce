841adb9c5e6c39ad851c20f2e6d11c4a73fa7559
htanwar <htanwar500@gmail.com>
2020-07-28 17:40:44 -0400

    update start

--- src/main_menu.py
+++ src/main_menu.py
@@ -4,3 +4,26 @@
 
 import pygame
 import os
+
+class mainMenu(object):
+    def __init__(self):
+        self.fontPlayScreen = pygame.font.Font('freesansbold.ttf', 40)
+        self.playScreenIntro = False
+
+    def tutorial(self, win):
+        self.playScreenIntro = True
+        continue_ = self.fontPlayScreen.render("Press [ Space ] to Start!", True, (255,255,255))
+        
+
+        while self.playScreenIntro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            win.fill((0,0,0))
+
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                self.playScreenIntro = False
+            win.blit(continue_, (400, 360))
+            pygame.display.update()
