5361fcc12c041e66f35057413df08fcdfcb2eae6
htanwar <htanwar500@gmail.com>
2020-08-05 13:42:34 -0400

    Add changes PR 8

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from src import player
 from src import main_menu
 from src import obstacles
 
+selected_character = 1
+
 def main():
     # Initialize Game
     pygame.init()
@@ -36,32 +38,33 @@ def main():
 
     jump_sound.set_volume(0.40)
     point_sound.set_volume(0.50)
-    explosion_sound.set_volume(0.50)
+    explosion_sound.set_volume(0.20)
 
     score = 0
     scoreCheck = True
     player1 = player.player_()
-
     mainMenu = main_menu.mainMenu()
 
     allObstacles = createObstacles()
     
     start_area = pygame.Rect(50,355, 180, 75)
-    settings_area = pygame.Rect(50,435, 180, 75)
-    quit_area = pygame.Rect(50,515, 180, 75)
+    characters_area = pygame.Rect(50,435, 180, 75)
+    settings_area = pygame.Rect(50,515, 180, 75)
+    quit_area = pygame.Rect(50,595, 180, 75)
     
     fontPlayScreen = pygame.font.Font("images/fonts/ALBAS.ttf", 40)
     logo = pygame.image.load("images/background/spaceForceLogo.png").convert_alpha()
     bkg = pygame.transform.scale(pygame.image.load("images/background/main_back.png").convert_alpha(), (2713, 720))
-    button_sprite = pygame.image.load("images/ui/GreenBtn1.png").convert_alpha()
+    button_sprite = pygame.transform.scale(pygame.image.load("images/ui/GreenBtn1.png").convert_alpha(), (230,80))
     
     start_ = fontPlayScreen.render("Start", True, (255,255,255))
+    characters_ = fontPlayScreen.render("Characters", True, (255,255,255))
     settings_ = fontPlayScreen.render("Options", True, (255,255,255))
     quit_game_ = fontPlayScreen.render("Exit", True, (255,255,255))
 
     
     click = False
-    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.set_volume(0.05)
     pygame.mixer.music.play(-1)
     while inmenu:
 
@@ -79,9 +82,11 @@ def main():
         win.blit(button_sprite, (50,355))
         win.blit(start_, (60, 360))
         win.blit(button_sprite, (50,435))
-        win.blit(settings_, (60, 440))
+        win.blit(characters_, (60, 440))
         win.blit(button_sprite, (50,515))
-        win.blit(quit_game_, (60, 520))
+        win.blit(settings_, (60, 520))
+        win.blit(button_sprite, (50,595))
+        win.blit(quit_game_, (60, 600))
         pygame.display.update()
         
         mx, my = pygame.mouse.get_pos()
@@ -91,6 +96,8 @@ def main():
             playing = True
         elif settings_area.collidepoint((mx,my)) and click:
             settings(win,clock)
+        elif characters_area.collidepoint((mx,my)) and click:
+            character_select(win,clock,player1)
         elif quit_area.collidepoint((mx,my)) and click:
             pygame.quit()
             sys.exit()
@@ -141,6 +148,71 @@ def main():
                 c_+=1
             mainMenu.game_over(win)
 
+def character_select(win,clock,player):
+    running = True
+    
+    selected1 = True
+    selected2 = False
+        
+    fontPlayScreen = pygame.font.Font("images/fonts/ALBAS.ttf", 40)
+    
+    bkg = pygame.transform.scale(pygame.image.load("images/background/main_back.png").convert_alpha(), (2713, 720))
+    button_sprite = pygame.image.load("images/ui/GreenBtn1.png").convert_alpha()
+    title_ = fontPlayScreen.render("Select Your Character", True, (255,255,255))
+    player1 = pygame.image.load("images/character/player1.png").convert_alpha()
+    player2 = pygame.image.load("images/character/player2.png").convert_alpha()
+    border = pygame.image.load("images/ui/selectborder.png").convert_alpha()
+    back_ = fontPlayScreen.render("Back", True, (255,255,255))
+    
+    back_area = pygame.Rect(50,600, 180, 75)
+    area1 = pygame.Rect(300,200, 247, 267)
+    area2 = pygame.Rect(700,220, 282, 250)
+    
+    click = False
+    
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN :
+                if event.button == 1:
+                    click = True
+
+        win.fill((0,0,0))
+        win.blit(bkg, (0,0))
+        win.blit(title_, (60, 50))
+        win.blit(player1, (300, 200))
+
+        win.blit(player2, (700, 220))
+
+        win.blit(button_sprite, (50,600))
+        win.blit(back_, (60, 605))
+        
+        if selected1:
+            win.blit(border, (230,150))
+        elif selected2:
+            win.blit(border, (650,150))        
+        
+        mx, my = pygame.mouse.get_pos()
+        if back_area.collidepoint((mx,my)) and click:
+            running = False
+        elif area1.collidepoint((mx,my)) and click:
+            selected1 = True
+            selected2 = False
+            player.image = pygame.transform.scale(pygame.image.load("images/character/player1.png").convert_alpha(), (player.width, player.height))
+            player.mask = pygame.mask.from_surface(pygame.image.load("images/character/player1.png").convert_alpha())
+        elif area2.collidepoint((mx,my)) and click:            
+            selected1 = False
+            selected2 = True
+            player.image = pygame.transform.scale(pygame.image.load("images/character/player2.png").convert_alpha(), (player.width, player.height))
+            player.mask = pygame.mask.from_surface(pygame.image.load("images/character/player2.png").convert_alpha())
+        click = False
+        
+        pygame.display.update()
+        clock.tick(60)
+
 def settings(win,clock):
     running = True
     
@@ -148,7 +220,7 @@ def settings(win,clock):
     
     bkg = pygame.transform.scale(pygame.image.load("images/background/main_back.png").convert_alpha(), (2713, 720))
     button_sprite = pygame.image.load("images/ui/GreenBtn1.png").convert_alpha()
-    start_ = fontPlayScreen.render("Options", True, (255,255,255))
+    options_ = fontPlayScreen.render("Options", True, (255,255,255))
     back_ = fontPlayScreen.render("Back", True, (255,255,255))
     
     back_area = pygame.Rect(50,600, 180, 75)
@@ -167,7 +239,7 @@ def settings(win,clock):
 
         win.fill((0,0,0))
         win.blit(bkg, (0,0))
-        win.blit(start_, (60, 50))
+        win.blit(options_, (60, 50))
         win.blit(button_sprite, (50,600))
         win.blit(back_, (60, 605))
         
@@ -183,7 +255,6 @@ def settings(win,clock):
 def createObstacles():
     '''
     Creates initial list of obstacles
-
     '''
     rocks = obstacles.obstacles()
 
@@ -202,7 +273,6 @@ def createObstacles():
 def checkCollisions(player, rocks):
     '''
     Checks if either of the obstacles collided with the player
-
     '''
     for r in rocks:
         dist1 = math.hypot( (int(player.x) + 63) - (r.x1 + r.width //2) , (int(player.y) + 63) - (r.y1 + r.width //2) )
