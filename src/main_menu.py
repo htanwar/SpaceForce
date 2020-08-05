ef2f92aae58f7dceacabcd61ba18b638e1d2d453
htanwar <htanwar500@gmail.com>
2020-08-05 16:39:00 -0400

    Add high score, and restart at gameover screen

--- src/main_menu.py
+++ src/main_menu.py
@@ -13,6 +13,7 @@ class mainMenu(object):
         self.bkg = pygame.transform.scale(pygame.image.load("images/background/main_back.png").convert_alpha(), (2713, 720))
         self.playScreenIntro = False
         self.gameOver = False
+        self.highScore = 0
 
     def tutorial(self, win):
         '''
@@ -35,7 +36,7 @@ class mainMenu(object):
             pygame.display.update()
       
     
-    def game_over(self, win):
+    def game_over(self, win, restart, menu):
         '''
         Shows the gameover screen
 
@@ -43,20 +44,35 @@ class mainMenu(object):
 
         self.gameOver= True
         continue_ = self.fontPlayScreen.render("Game Over!", True, (255,255,255))
-
+        click = False
         while self.gameOver:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            win.fill((0,0,0))
+                if event.type == pygame.MOUSEBUTTONDOWN :
+                    if event.button == 1:
+                        click = True
+            mx, my = pygame.mouse.get_pos()
                         
             win.blit(continue_, (520, 360))
             pygame.display.update()
+
+            if restart.collidepoint((mx,my)) and click:
+                return (True, False)
+            if menu.collidepoint((mx,my)) and click:
+                return (False, True)
+            
     
     def score(self, win, s):
         '''
         Displays the score
         '''
+        if s > self.highScore:
+            self.highScore = s
         continue_ = self.fontPlayScreen.render("Score: "+str(s), True, (255,255,255))
         win.blit(continue_, (20, 20))
+    
+    def high_score(self, win):
+        continue_ = self.fontPlayScreen.render("High Score: "+ str(self.highScore), True, (255,255,255))
+        win.blit(continue_, (510, 450))
